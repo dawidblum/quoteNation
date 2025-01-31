8e8d0ff6326c6b9cab73bd9f8e6f219986fc3f9e
Dawid Blumczynski <codeblumi@proton.me>
2025-01-31 21:28:23 +0100

    Initial commit

---
+++ src/test/kotlin/com/codeblumi/quotenation/QuoteNationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.codeblumi.quotenation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuoteNationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
